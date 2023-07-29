881d19974ad766a76cec5f283dffcaf54589442b
Sarah <83086706+smsgoldberg@users.noreply.github.com>
2023-07-29 17:38:21 -0400

    restart work on functions

--- tic-tac-toe-v2/js/main.js
+++ tic-tac-toe-v2/js/main.js
@@ -1,14 +1,33 @@
 /*----constants---*/
+const COLORS = {
+    0: 'black',
+    1: '00F1FF',
+    '-1': 'ff901f'
+}
 
+const winningCombos = {
+   [ [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+}
 
 /*-----state variables ----*/
-let board 
-let turn
-let winner
-
+let board //this array holds the squares
+let turn //enables us to remember whose turn it is
+let winner //represents 3 different possibilities -- a winner, a tie, or a game in progress
 
 /*---cached elements---*/
 
+//enables us to change what is displayed on the message board above the game board
+const messageBoard = document.querySelector('h2');
+
+const playAgainButton = document.querySelector('button');
 
 /*----functions----*/
 
